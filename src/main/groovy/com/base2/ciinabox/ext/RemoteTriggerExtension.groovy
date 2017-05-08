653e6b9d559c87012e939290f242d0191b859940
Nikola Tosic <tosicnikola10@gmail.com>
2017-05-08 10:34:29 +1000

    Remote trigger feature implemented using ExtensionBase

--- src/main/groovy/com/base2/ciinabox/ext/RemoteTriggerExtension.groovy
+++ src/main/groovy/com/base2/ciinabox/ext/RemoteTriggerExtension.groovy
@@ -5,34 +5,23 @@ import javaposse.jobdsl.dsl.Job
 /**
  * Created by nikolatosic on 8/05/2017.
  */
-class RemoteTriggerExtension implements ICiinaboxExtension {
-
-//  @Override
-//  String getConfigurationKey() {
-//    'remoteTrigger'
-//  }
-//
-//  @Override
-//  Map getDefaultConfiguration() {
-//    return [:]
-//  }
-//
-//  @Override
-//  void extendDsl(Job job, Object extensionConfiguration, Object jobConfiguration) {
-//    job.configure {
-//      project ->
-//        (project / 'authToken').setValue(extensionConfiguration['token'])
-//    }
-//  }
+class RemoteTriggerExtension extends ExtensionBase {
 
   @Override
-  void extend(Job job, Object jobConfiguration) {
-    if(jobConfiguration.get('remoteTrigger')){
-      def extensionConfiguration = jobConfiguration.get('remoteTrigger')
-      job.configure {
-        project ->
-          (project / 'authToken').setValue(extensionConfiguration['token'])
-      }
+  String getConfigurationKey() {
+    'remoteTrigger'
+  }
+
+  @Override
+  Map getDefaultConfiguration() {
+    return [:]
+  }
+
+  @Override
+  void extendDsl(Job job, Object extensionConfiguration, Object jobConfiguration) {
+    job.configure {
+      project ->
+        (project / 'authToken').setValue(extensionConfiguration['token'])
     }
   }
 }
